203a0ca90c8df606ab7ab0a18f81b2eed1f3c9c6
Intrepidd <adrien@siami.fr>
2012-10-20 16:27:23 +0100

    Adding spec & code for GithubMatesFetcher.get_users

--- lib/github_mates_fetcher.rb
+++ lib/github_mates_fetcher.rb
@@ -2,12 +2,35 @@ class GithubMatesFetcher
   attr_accessor :username
   attr_accessor :repository
 
+  # Gets informations about a list of users
+  #
+  # @param [Array] usernames An array of usernames
+  # @return [Array] An array of hashes, containing the informations about each user given by the github API
+  def self.get_users(usernames)
+    result = []
+    usernames.each do |username|
+        user = Github.users.get(:user => username) rescue nil
+        if user.present?
+          result << user.to_hash
+        end
+    end
+    result
+  end
+
+  # Create an instance of GithubMatesFetcher
+  #
+  # @param [String] username the username
+  # @param [String] repository The name of the repository
   def initialize(username, repository)
     self.username = username
     self.repository = repository
   end
 
-  def get_commiters
+  # Gets the committers of this repository
+  #
+  # @return [Array, Nil] An array of string containing the logins of the commiters, or nil if the user / repo
+  # couple does not exist
+  def get_committers
     begin
       commits = Github.repos.commits.all self.username, self.repository
     rescue
--- spec/models/github_mates_fetcher_spec.rb
+++ spec/models/github_mates_fetcher_spec.rb
@@ -11,14 +11,32 @@ describe GithubMatesFetcher do
   describe '#commiters' do
     context "Valid repository" do
       fetcher = GithubMatesFetcher.new 'Intrepidd', 'rails-canhaz'
-      fetcher.get_commiters.should == ['Intrepidd']
+      fetcher.get_committers.should == ['Intrepidd']
     end
 
     context "Invalid repository" do
       fetcher = GithubMatesFetcher.new 'Intrepidd', 'does-not-exist'
-      fetcher.get_commiters.should == nil
+      fetcher.get_committers.should == nil
     end
 
+  end
+
+  describe '.get_users' do
+    context "One user" do
+      users = GithubMatesFetcher.get_users(['Intrepidd'])
+      users.size.should == 1
+      users.first['login'].should == 'Intrepidd'
+    end
+
+    context "Wrong user" do
+      users = GithubMatesFetcher.get_users(['this_user_does_not_exist_at_least_i_hope'])
+      users.should == []
+    end
+
+    context "Get multiple users" do
+      users = GithubMatesFetcher.get_users(['Intrepidd', 'fat', 'random_user_non_existing', 'rails'])
+      users.size.should == 3
+    end
   end
 
 end

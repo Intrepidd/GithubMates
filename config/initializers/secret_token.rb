7f6ac4ca6b66446eb2230038a97a221a61f96a03
Intrepidd <adrien@siami.fr>
2012-10-20 11:18:41 +0100

    Initial rails import w/ some gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubMates::Application.config.secret_token = 'a1e547a86d686b40c0a767b48be5efc2b70c0da91c5bd0cc7f8f728b7f4c05ff55ece175cad41b0261a2f30b087f3db6e548ba3542d159130c87cb06fe3731b2'
